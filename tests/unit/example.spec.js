0b66e775dc63016c859c66f23c574f7e54e10d0b
Joshua Mora <joshuaalexandermora@gmail.com>
2020-12-23 17:46:55 -0400

    Starting tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,1 +1,13 @@
+import { mount } from '@vue/test-utils'
+import AppHeader from '../../src/components/AppHeader/AppHeader.vue'
 
+describe('Foo', () => {
+  it('renders a message and responds correctly to user input', () => {
+    const $route = { path: '/' }
+    const wrapper = mount(AppHeader, { mocks: $route })
+
+    expect(wrapper.contains('.app-header').length).toBe(1)
+    // expect(wrapper.find('h1').text()).toEqual('TODO-APP')
+    // expect(wrapper.vm.$route.path).toEqual($route.path)
+  })
+})
